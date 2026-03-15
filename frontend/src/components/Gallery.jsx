c208639c48b76effb5f5cccf860d5a8bff860be6
Tamilselvanoffl <tamilan112000@gmail.com>
2026-03-16 03:34:29 +0530

    Updated Gallery component

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -7,7 +7,7 @@ function Gallery() {
 
  const fetchImages = async () => {
 
-  const res = await axios.get("http://localhost:5000/api/images");
+  const res = await axios.get("https://gallery-module-backend.onrender.com/api/images");
 
   setImages(res.data);
 
