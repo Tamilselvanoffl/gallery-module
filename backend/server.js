10290cea21989841f080645e960a5f5ea2542e52
Tamilselvanoffl <tamilan112000@gmail.com>
2026-03-17 01:51:16 +0530

    fix wrong route import

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,7 @@ const galleryRoutes = require("./routes/galleryRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://gallery-module-backend.onrender.com"],
-    methods: ["GET", "POST", "DELETE"],
-    allowedHeaders: ["Content-Type"]
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -24,6 +17,10 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log("MongoDB connection failed:", err.message));
+
+app.get("/", (req,res)=>{
+ res.send("Gallery Backend Running");
+});
 
 app.use("/api", galleryRoutes);
 
